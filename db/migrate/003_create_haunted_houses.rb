0f3bdb680c32c01d050830d09a40343d3b09bcf6
Michael Harris <mwharris7@gmail.com>
2020-07-02 12:21:03 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,14 @@
 # Create your haunted_houses migration here
+class CreateCostumes < ActiveRecord::Migration[5.2]
+
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.integer :price
+      t.string :size
+      t.string :image_url
+      t.datetime :created_at
+      t.datetime :updated_at
+    end
+  end
+end
